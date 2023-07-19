9fe209811e808483c743011078be022f7f659f3c
felgueres <pablofelgueres@gmail.com>
2023-07-19 13:27:50 -0700

    adding latency test

--- gpt.py
+++ gpt.py
@@ -3,6 +3,8 @@ import openai
 import backoff
 from extra.logger_config import setup_logger
 import pprint
+import json
+
 logger = setup_logger(__name__)
 
 from dotenv import load_dotenv
@@ -22,19 +24,23 @@ retriable_exceptions = (openai.error.APIError, openai.error.Timeout, openai.erro
 def non_retriable(e):
     return not isinstance(e, retriable_exceptions)
 
-def handle_success(details):
-    pprint.pprint(details)
+def handle_success(d):
+    l = { "elapsed": d['elapsed'], 
+          "model_type": 'fn' if 'functions' in d['kwargs'] else 'compl'}
+    with open('./latency.jsonl', 'a') as f:
+        f.write(json.dumps(l) + '\n')
+    pprint.pprint(d)
 
 @backoff.on_exception(backoff.expo, openai.error.OpenAIError, max_tries=5, giveup=non_retriable, on_success=handle_success)
 def completions_with_backoff(**kwargs):
     return openai.ChatCompletion.create(**kwargs, timeout=2)
 
-def gpt(prompt, model='gpt-3.5-turbo-0613', temperature=0.5, max_tokens=1000, n=1, stop=None):
-    messages = [{'role': 'user', 'content': prompt}]
+def gpt(prompt, model="gpt-3.5-turbo-0613", temperature=0.5, max_tokens=1000, n=1, stop=None):
+    messages = [{"role": "user", "content": prompt}]
     res = completions_with_backoff(messages=messages, model=model, temperature=temperature, max_tokens=max_tokens, n=n, stop=stop)
     return res
 
-def gpt_w_function(prompt, functions, model='gpt-3.5-turbo-0613', temperature=0.5, max_tokens=1000, n=1, stop=None):
-    messages = [{'role': 'user', 'content': prompt}]
+def gpt_w_function(prompt, functions, model="gpt-3.5-turbo-0613", temperature=0.5, max_tokens=1000, n=1, stop=None):
+    messages = [{"role": "user", "content": prompt}]
     res = completions_with_backoff(messages=messages, functions=functions, model=model, temperature=temperature, max_tokens=max_tokens, n=n, stop=stop)
     return res
---
+++ test_latency.py
@@ -0,0 +1,46 @@
+# script to test gpt w and wo function 
+from gpt import gpt_w_function, gpt
+from extra.utils import load_prompt
+from widgets.core import match_fn
+
+q = "What is the weather in SF?"
+
+def classifier(q):
+    prompt = load_prompt('classifier')['prompt'].format(q=q, categories=match_fn.keys())
+    pred_widget_cls = gpt(prompt=prompt)["choices"][0]["message"]["content"]
+    return pred_widget_cls
+
+def extract(q):
+    prompt = load_prompt('extract_params')['weather']['prompt'].format(q=q)
+    pred_location = gpt(prompt=prompt)["choices"][0]["message"]["content"]
+    return pred_location
+
+def fn_cls_and_extract(q): 
+    categories = list(match_fn.keys())
+    fns = [{
+        "name": "get_current_weather",
+        "description": "Get current weather in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "City name or zip code, e.g. San Francisco, CA"
+                },
+                "category": {
+                    "type": "string",
+                    "description": "Given a sentence, predict category",
+                    "enum": categories 
+                }
+            },
+            "required": ["location", "category"]
+        }
+    }] 
+    prompt = load_prompt('fn')['prompt'].format(q=q)
+    res = gpt_w_function(prompt=prompt, functions=fns)['choices'][0]['message']['function_call']['arguments']
+    return res
+
+n_tries = 10
+for i in range(n_tries): 
+    fn_cls_and_extract(q)
+    classifier(q)
